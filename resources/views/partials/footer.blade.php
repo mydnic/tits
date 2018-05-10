f24fb57e0ca802bf653a720030b227626e9d5d6d
Clément Rigo <rigoclement@mydnic.be>
2018-05-10 10:58:17 +0200

    KISS

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,30 +1,8 @@
-<footer class="footer-area section-gap">
+<footer class="footer">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-2  col-md-6">
-                <div class="single-footer-widget">
-                    <h6>Top Products</h6>
-                    <ul class="footer-nav">
-                        <li><a href="https://webradio.io">WebRadio.io</a></li>
-                        <li><a href="http://dailyscrum.tips">Daily Scrum Tips</a></li>
-                        <li><a href="https://24imagesparseconde.com">24 Images par Seconde</a></li>
-                        <li><a href="https://mydnic.be/">Mydnic.be</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-4  col-md-6">
-                <div class="single-footer-widget mail-chimp">
-                    <h6 class="mb-20">Contact Us</h6>
-                    <p>
-                        Avenue Gabriel Emile Lebon 105 bt. 7<br>1160 Auderghem<br>BE
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="row footer-bottom d-flex justify-content-between">
-            <p class="col-lg-8 col-sm-12 footer-text m-0">
-                Copyright &copy; {{ date('Y') }} All rights reserved
+        <div class="content has-text-centered">
+            <p>
+                Tech In T-Shirt &copy; {{ date('Y') }}
             </p>
         </div>
     </div>
